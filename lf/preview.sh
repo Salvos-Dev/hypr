cf31e93358d733e4859c84f2ebc5c561cd1b0d9b
Salvos-Dev <dev-salvos@proton.me>
2025-06-28 20:01:50 -0600

    Added archive preview

--- lf/preview.sh
+++ lf/preview.sh
@@ -4,16 +4,32 @@
 ext="${1##*.}"
 
 # Get the dimensions of the preview pane
+path="$1"
 width="$2"
 height="$3"
 
 case "$(file -b --mime-type "$1")" in
+    # --- IMAGES ---
     image/*)
-        chafa -f sixels -s "${width}x${height}" "$1"
+        # Use chafa for sixel image previews in a compatible terminal (like foot)
+        chafa -f sixels -s "${width}x${height}" -- "$path"
         ;;
+
+    # --- ARCHIVES ---
+    application/zip|application/x-tar|application/x-gzip|application/x-bzip2|application/x-7z-compressed|application/x-rar)
+        # Use atool to list contents, pipe to bat for nice formatting
+        atool -l -- "$path" | bat --language=sh --color=always
+        ;;
+
+    # --- TEXT / CODE ---
+    text/*|application/json|application/x-shellscript)
+        # Use bat for syntax-highlighted previews of text files
+        bat --color=always --plain --line-range :500 -- "$path"
+        ;;
+
+    # --- FALLBACK for other file types ---
     *)
-        # For other file types, you can add other previewers here
-        # For example, for text files:
-        bat --color=always --plain "$1"
+        # Show basic file info for anything else
+        file "$path"
         ;;
 esac
